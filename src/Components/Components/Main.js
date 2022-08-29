cd0e16f611dd375f5c841da79ac0f95d43e952ee
Roland <cook.rolandcook.roland@gmail.com>
2022-08-29 16:06:45 -0400

    Finished backend
    called api and linked everything up.

--- src/Components/Components/Main.js
+++ src/Components/Components/Main.js
@@ -3,61 +3,87 @@ import planetone from "./planet-one.png"
 import React from 'react';
 import {useState} from 'react';
 import $ from 'jquery'
-let ids =document.getElementById("input-text")
-
+import axios from "axios";
+let test = document.getElementById("input-text");
 
 
 
 
 
 const Main = ()=>{
-    
-    let names =ids.value
-
-    $.ajax({
-        method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/planets?name=' + names,
-        headers: { 'X-Api-Key': 'LUzifa1Hcd3nt987OZgdkA==8nL9HBP8W1PZUT84'},
-        contentType: 'application/json',
-        success: function(result) {
-            console.log(result);
-        },
-        error: function ajaxError(jqXHR) {
-            console.error('Error: ', jqXHR.responseText);
-        }
-    });
-
-
+  
+   // using jupiter as reference for planet size/ using our sun as reference for star size and temp
+   
     // Use states
-      const [name, setName] = useState('Name');
+      const [message, setMessage] = useState("Name");
+      const [name, setName] = useState("Name");
+
       const [Mass, setMass] = useState('Mass');
       const [Radius, setRadius] = useState('Radius');
       const [Temp, setTemp] = useState('Temp');
       const [years, setYears] = useState('Light Years Away:');
       const [star, setStar] = useState('Host Star Size:');
       const [startemp, setStarTemp] = useState('Host Star Temperature:');
+//
 
+
+
+      const handleChange = event => {
+
+        setMessage(event.target.value)
+        
+      };
 
       const handleClick = event => {
         event.preventDefault();
-    
+
+        var name = message
+        $.ajax({
+            method: 'GET',
+            url: 'https://api.api-ninjas.com/v1/planets?name=' + name,
+            headers: { 'X-Api-Key': 'LUzifa1Hcd3nt987OZgdkA==8nL9HBP8W1PZUT84'},
+            contentType: 'application/json',
+            success: function(result) {
+                
+                console.log(result);
+
+                
         // 👇️ set value of input field
-        setName(ids.value);
-       
+        setMass("Mass: " +result[0].mass)
+        setRadius("Radius: "+result[0].radius)
+        setTemp("Temperature: "+result[0].temperature) 
+        setYears("Light Years Away: "+result[0].distance_light_year)
+        setStar("Star Size: "+result[0].host_star_mass)
+        setStarTemp("Star Temperature: "+result[0].host_star_temperature)
+        setMessage("Planet Name: " + result[0].name)
+
+
+
+            },
+            error: function ajaxError(jqXHR) {
+                console.error('Error: ', jqXHR.responseText);
+            }
+
+            
+        });
+        
+    
+
+     
       }
-      
 
 
 
     return(
     
     <div className="main">
+      
         <div className="planet-name">
-        <h1 className="Name">{name}</h1>
+        <h1 className="Name">{message}</h1>
         <div className="search-bar">
-        <form className="example "  action="action_page.php" >
-        <input type="text" placeholder="Search.." name="search" id="input-text"/>
-        <button type="submit" onClick={handleClick} id="btn-search"><i className="fa-solid fa-satellite-dish"></i></button>
+        <form className="example "  action="action_page.php" onSubmit={handleClick} >
+        <input type="text" placeholder="Search.." name="search" id="input-text" onChange={handleChange} />
+        <button type="submit"  id="btn-search"><i className="fa-solid fa-satellite-dish"></i></button>
         </form>
         </div>
 
